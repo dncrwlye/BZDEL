b53deab3688ad5964d9bc177fd381fd15749569d
Alex Washburne <bigalculus@gmail.com>
2017-08-17 14:04:27 -0600

    parallelization of null phylofactorization

--- R/Olival_Phylofactorization.R
+++ R/Olival_Phylofactorization.R
@@ -2,7 +2,18 @@
 library(RCurl)
 library(ape)
 library(phylofactor)
+library(parallel)
 source('R/fisherFactor.R')
+parpf <- function(reps,pf,phat){
+  Pvals <- matrix(NA,nrow=reps,ncol=pf$nfactors)
+  for (nn in 1:reps){
+    Z <- rbinom(Ntip(pf$tree),size=1,prob=phat)
+    names(Z) <- pf$tree$tip.label
+    pf <- fisherFactor(Z,pf$tree,nfactors=10)
+    Pvals[nn,] <- pf$pvals
+  }
+  return(Pvals)
+}
 
 load('Workspaces/el_jefe.rdata')
 VOlival <- read.csv('Data/HP3/data/viruses.csv')
@@ -23,23 +34,28 @@ pf.IsZoonotic$factors
 nms <- lapply(pf.IsZoonotic$bins,FUN=function(a,tree) tree$tip.label[a],tree=tree)
 
 
-reps=100
-Pvals <- matrix(NA,nrow=reps,ncol=pf.IsZoonotic$nfactors)
-for (nn in 1:reps){
-  Z <- rbinom(Ntip(tree),size=1,prob=phat)
-  names(Z) <- tree$tip.label
-  pf.IsZoonotic.null <- fisherFactor(Z,tree,nfactors=10)
-  Pvals[nn,] <- pf.IsZoonotic.null$pvals
+################ Null Simulation ##############
+ncores=7
+reps=50
+cl <- phyloFcluster(ncores)
+clusterExport(cl,varlist = c('fisherFactor','parpf'))
+reps=as.list(rep(reps,ncores))
+Ps <- parLapply(cl,reps,fun=parpf,pf=pf.IsZoonotic,phat=mean(VOlival$IsZoonotic))
+stopCluster(cl)
+for (i in 1:length(Ps)){
+  if (i==1){Pvals <- Ps[[i]]} else{Pvals <- rbind(Pvals,Ps[[i]])}
 }
+rm('Ps')
 
 tiff('Figures/Default_FisherFactors_of_IsZoonotic.tiff',height=800,width=1600)
   par(mfrow=c(1,2))
   binPhyloPlot(pf.IsZoonotic,factor=8,type='radial',use.edge.length=F,edge.width=2,main='Phylogenetic Factors of Spillover',cex.main=3,cex.lab=2)
-  plot(pf.IsZoonotic$pvals,log='y',type='l',lwd=2,col='black',ylim=c(1e-8,1),xlab='Factor',ylab='P-value',main='Fisher-test Phylofactorization',cex.main=3,cex.lab=2,cex.axis=2)
-  for (nn in 1:reps){
-    lines(Pvals[nn,],col='grey',lwd=0.5)
+  plot(pf.IsZoonotic$pvals,log='y',type='l',lwd=2,col='red',ylim=c(1e-8,1),xlab='Factor',ylab='P-value',main='Fisher-test Phylofactorization',cex.main=3,cex.lab=2,cex.axis=2)
+  for (nn in 1:nrow(Pvals)){
+    lines(Pvals[nn,],col=adjustcolor('black',alpha.f=0.1))
   }
-  lines(pf.IsZoonotic$pvals,lwd=2,col='black')
+  # matplot(t(Pvals),col='grey',type='l')
+  lines(pf.IsZoonotic$pvals,lwd=2,col='red')
   legend(4,1e-4,legend=c('Olival et al. Data','Null Simulations'),col=c('black','grey'),lwd=c(2,1))
 dev.off()
 
@@ -57,21 +73,20 @@ probs <- lapply(Clades,FUN=function(b,Z) mean(Z[b]),Z)
 ##Then, a bunch of Unavirus, equine encaphalitis etc., have P=0.64,
 ##finally, Bovine leukemia and three primate T=lymphotropic viruses have P=1
 
-library(xlsx)
+# library(xlsx)
+# 
+# for (i in 1:11){
+#   if (i<11){
+#     nm <- paste('Factor ',toString(i),', phat=',toString(probs[[i]]),sep='')
+#   } else {
+#     nm <- paste('Paraphyletic Remaidner, phat=',toString(probs[[i]]),sep='')
+#   }
+#   write.xlsx(VOlival[VOlival$vVirusNameCorrected %in% CladeNames[[i]],],file='Data/Olival_Taxonomic_phylofactors.xlsx',
+#              append=(!(i==1)),sheetName = nm)
+# }
 
-for (i in 1:11){
-  if (i<11){
-    nm <- paste('Factor ',toString(i),', phat=',toString(probs[[i]]),sep='')
-  } else {
-    nm <- paste('Paraphyletic Remaidner, phat=',toString(probs[[i]]),sep='')
-  }
-  write.xlsx(VOlival[VOlival$vVirusNameCorrected %in% CladeNames[[i]],],file='Data/Olival_Taxonomic_phylofactors.xlsx',
-             append=(!(i==1)),sheetName = nm)
-}
-
-
+########## creating phylobin variable ##############
 DF <- VOlival
-
 DF$phyloBin <- numeric(nrow(DF))
 for (i in 1:11){
   DF$phyloBin[DF$vVirusNameCorrected %in% CladeNames[[i]]] <- i
@@ -79,8 +94,18 @@ for (i in 1:11){
 DF$phyloBin <- as.factor(DF$phyloBin)
 
 write.xlsx(DF,file='Data/Olival_Dataset_with_phyloBin_Variable.xlsx')
-
 save(list=ls(),file='Virus_Phylofactorization_workspace')
+
+
+
+
+
+
+
+
+
+
+
 
 # Phylofactorization by Human-Association ---------------------------------
 
@@ -95,24 +120,27 @@ HumanBinNms <- lapply(HumanBins,FUN=function(a,b) b[a],b=names(Z))
 
 
 ########### Null factors
+ncores=7
 reps=100
-phat <- mean(Z)
-HPvals <- matrix(NA,nrow=reps,ncol=length(pf.IsZoonotic$pvals))
-for (nn in 1:reps){
-  Z <- rbinom(Ntip(tree),size=1,prob=phat)
-  names(Z) <- tree$tip.label
-  pf.IsZoonotic.null <- fisherFactor(Z,tree,nfactors=10)
-  HPvals[nn,] <- pf.IsZoonotic.null$pvals
+cl <- phyloFcluster(ncores)
+clusterExport(cl,varlist = c('fisherFactor','parpf'))
+reps=as.list(rep(reps,ncores))
+Ps <- parLapply(cl,reps,fun=parpf,pf=pf.IsHoSa,phat=mean(VOlival$IsHoSa))
+stopCluster(cl)
+for (i in 1:length(Ps)){
+  if (i==1){Pvals <- Ps[[i]]} else{Pvals <- rbind(Pvals,Ps[[i]])}
 }
+rm('Ps')
+
 
 tiff('Figures/Default_FisherFactors_of_IsHoSa.tiff',height=800,width=1600)
   par(mfrow=c(1,2))
   binPhyloPlot(pf.IsHoSa,factor=8,type='radial',use.edge.length=F,edge.width=2,main='Phylogenetic Factors of Human Viruses',cex.main=3,cex.lab=2)
-  plot(pf.IsHoSa$pvals,log='y',type='l',lwd=2,col='black',ylim=c(1e-8,1),xlab='Factor',ylab='P-value',main='Fisher-test Phylofactorization',cex.main=3,cex.lab=2,cex.axis=2)
+  plot(pf.IsHoSa$pvals,log='y',type='l',lwd=2,col='red',ylim=c(1e-8,1),xlab='Factor',ylab='P-value',main='Fisher-test Phylofactorization',cex.main=3,cex.lab=2,cex.axis=2)
   for (nn in 1:reps){
-    lines(HPvals[nn,],col='grey',lwd=0.5)
+    lines(HPvals[nn,],col='grey',col=adjustcolor('black',alpha.f=0.1))
   }
-  lines(pf.IsHoSa$pvals,lwd=2,col='black')
+  lines(pf.IsHoSa$pvals,lwd=2,col='red')
   legend(4,1e-4,legend=c('Olival et al. Data','Null Simulations'),col=c('black','grey'),lwd=c(2,1))
 dev.off()
 
