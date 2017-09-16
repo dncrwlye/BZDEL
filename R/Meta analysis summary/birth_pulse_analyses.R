ecc78c872a7efb057c8333bf5c02bf3fbf9b5c5c
Dan Crowley <dancrowley.e@gmail.com>
2017-09-16 12:59:44 -0600

    adding a unite function that i think improves things

--- R/Meta analysis summary/birth_pulse_analyses.R
+++ R/Meta analysis summary/birth_pulse_analyses.R
@@ -4,7 +4,7 @@ library(readxl)
 library(binom)
 library(lubridate)
 
-MetaAnalysis_Data_New_Version_ <- read_excel("~/Dropbox_gmail/Dropbox/bat virus meta-analysis/MetaAnalysis Data New Version .xlsx", 
+MetaAnalysis_Data_New_Version <- read_excel("~/Dropbox_gmail/Dropbox/bat virus meta-analysis/MetaAnalysis Data New Version.xlsx", 
                                                   col_types = c("text", "numeric", "text", 
                                                                           "text", "text", "text", "text", "text", 
                                                                           "text", "text", "text", "text", "text", 
@@ -20,10 +20,7 @@ MetaAnalysis_Data_New_Version <- MetaAnalysis_Data_New_Version %>%
   mutate(virus = trimws(virus)) %>%
   mutate(species = trimws(species)) 
 
-
-
-
-seroprevalence <- MetaAnalysis_Data_New_Version_ %>%
+seroprevalence <- MetaAnalysis_Data_New_Version %>%
   filter(outcome == 'Seroprevalence') %>%
   select(title, last_name_of_first_author, virus, study_type, study_design, methodology, species, sex, age_class, sampling_location, sample_size, seroprevalence_percentage, single_sampling_point, sampling_date_single_time_point, start_of_sampling, end_of_sampling) %>%
   mutate(virus = ifelse((virus == "Ebola" | 
@@ -40,23 +37,22 @@ seroprevalence <- MetaAnalysis_Data_New_Version_ %>%
   mutate(country = stri_extract_first_regex(sampling_location, '[a-z]+')) %>%
   mutate(successes = round((1/100)*seroprevalence_percentage * sample_size,0))
 
-
 species_location_table <- seroprevalence %>%
   group_by(species, country) %>%
   summarise(sum = sum(sample_size))
-  
 
-filovirus.seroprevalence<-seroprevalence %>%
-  filter(virus=='Filovirus')
+filovirus.seroprevalence<-seroprevalence #%>%
+  #filter(virus=='Filovirus')
 
 seroprevalence_henipavirus_single_time_point <- filovirus.seroprevalence %>%
     filter(single_sampling_point == 1) %>%
     mutate(month = round_date(sampling_date_single_time_point, unit= 'months')) %>%
     mutate(month = as.numeric(format(month, "%m"))) %>%
     #mutate(month = format(as.Date(sampling_date_single_time_point), "%m-%d"))) %>%
-    group_by(month, age_class, title, species, country, last_name_of_first_author, global_location) %>%
+    group_by(month, age_class, title, species, virus, country, last_name_of_first_author, methodology, global_location, sampling_location) %>%
     summarise(sample_size = sum(sample_size), successes = sum (successes))  %>%
     mutate(seroprevalence_per = 100 * (successes/sample_size)) %>%
+    filter(!is.na(seroprevalence_per)) %>%
     mutate(seroprevalence_per_upper_bound = 100*binom.exact(successes, sample_size, conf.level = .95)$upper) %>%
     mutate(seroprevalence_per_lower_bound = 100*binom.exact(successes, sample_size,conf.level = .95)$lower) %>%
     ungroup() 
@@ -69,7 +65,7 @@ seroprevalence_henipavirus_unclear_time_point <- filovirus.seroprevalence %>%
     #mutate(month = round_date(sampling_date_single_time_point, unit= 'months')) %>%
     mutate(month_1 = ifelse(difftime < 364, as.numeric(format(start_of_sampling, "%m")), 1)) %>%
     mutate(month_2 = ifelse(difftime < 364, as.numeric(format(end_of_sampling, "%m")), 12)) %>%
-    group_by(month_1, month_2, age_class, species, country, title, last_name_of_first_author, global_location) %>%
+    group_by(month_1, month_2, age_class, species, virus, country, title, last_name_of_first_author, methodology, global_location, sampling_location) %>%
     summarise(sample_size = sum(sample_size), successes = sum (successes),difftime = mean(difftime))%>%
     mutate(difftime_sqrt = sqrt(difftime)) %>% #going to divide any long sampling period by the square root of the sampling period
     mutate(seroprevalence_per = 100 * (successes/sample_size)) %>%
@@ -83,8 +79,8 @@ seroprevalence_henipavirus_unclear_time_point <- filovirus.seroprevalence %>%
     select(-c(difftime, difftime_sqrt)) %>%
     mutate(title = paste(title, "xc"))
   
-  x <- seroprevalence_henipavirus_unclear_time_point[,c(2:13)]
-  y <-seroprevalence_henipavirus_unclear_time_point[,c(1,3:13)]
+  x <- seroprevalence_henipavirus_unclear_time_point[,c(2:ncol(seroprevalence_henipavirus_unclear_time_point))]
+  y <-seroprevalence_henipavirus_unclear_time_point[,c(1,3:ncol(seroprevalence_henipavirus_unclear_time_point))]
   colnames(y) <- colnames(x)
   seroprevalence_henipavirus_unclear_time_point <- rbind(y,x) %>%
     rename(month = month_2)
@@ -93,8 +89,9 @@ seroprevalence_henipavirus_unclear_time_point <- filovirus.seroprevalence %>%
   #colnames(seroprevalence_henipavirus_unclear_time_point) <- colnames(seroprevalence_henipavirus_single_time_point)
 seroprevalence_graph <- rbind(seroprevalence_henipavirus_unclear_time_point, seroprevalence_henipavirus_single_time_point)
   
-x<-as.data.frame(table(seroprevalence_graph$species, seroprevalence_graph$country)) %>%
-  filter(Freq != 0)
+species_location_table_graph <- seroprevalence_graph %>%
+  group_by(species, country) %>%
+  summarise(sum = sum(sample_size))
 
 #making the adjustments to our time series data
 seroprevalence_graph <- seroprevalence_graph %>%
@@ -102,22 +99,28 @@ seroprevalence_graph <- seroprevalence_graph %>%
                                ifelse(species == 'Eidolon helvum', month - 11.5, #this is so effing sketch, the ranges of when this bat are so drastic. I don't have estimates for Zambia (apparently there are 'too many crocodiles', ~*eye roll*~), so I'm going to try Malawi...oh god
                                ifelse(species == 'Epomops franqueti', month - 1, #no estimate for our location (gabon), so using the DRC estimate
                                ifelse(species == "Hypsignathus monstrosus", month -2, #using the gabon estimate for DRC and gabon)
-                               ifelse(species == "Myonycteris torquata", month - 2, NA))))))
+                               ifelse(species == "Pteropus lylei", month - 2, #using Wacha... estimate for Thailand for both Thailand and Cambodia
+                               ifelse(species == "Pteropus hypomelanus", month - 4.5, # no estimate for Malaysia or Thailand, so Im using the Phillipines estimate, probably way off      
+                               ifelse((species == "Pteropus vampyrus" & country == 'malaysia')|(species == "Pteropus vampyrus" & country == 'thailand'), month - 3.4,
+                               ifelse(species == "Pteropus vampyrus" & country == 'indonesia', month - 4.5,
+                  
+                               ifelse(species == "Myonycteris torquata", month - 2, NA))))))))))
 
 seroprevalence_graph %>%
   filter(!is.na(month.dc.birthpulse)) %>%
+  unite(group, c(age_class, species, country, title, methodology), sep = " ") %>%
   ungroup() %>%
   ggplot() +
-  geom_point(aes(x= month.dc.birthpulse, y= seroprevalence_per, colour = species, group = species)) +
-  geom_line(lwd=1, aes(x= month.dc.birthpulse, y=seroprevalence_per, colour = species, group = species)) +
-  #geom_ribbon(alpha = .2, aes(x= month.dc.birthpulse, 
-                              #ymin=seroprevalence_per_lower_bound, 
-                              #ymax=seroprevalence_per_upper_bound,
-                              #fill= title)) +
-  #theme(legend.position="none") +
+  geom_point(aes(x= month.dc.birthpulse, y= seroprevalence_per, colour = group, group = group)) +
+  geom_line(lwd=1, aes(x= month.dc.birthpulse, y=seroprevalence_per, colour = group, group = group)) +
+  geom_ribbon(alpha = .2, aes(x= month.dc.birthpulse, 
+                              ymin=seroprevalence_per_lower_bound, 
+                              ymax=seroprevalence_per_upper_bound,
+                              fill= group)) +
+  theme(legend.position="none") +
   ylab('seroprevalence') +
   ggtitle(paste(c('Filovirus', "Seroprevalence; Data from Meta Analysis")))+
-  facet_wrap(~age_class)
+  facet_wrap(~virus)
 
 
 
