7b3162ac26f1c0fe6d3639c4e720d8ec887696ca
Dan Crowley <dancrowley.e@gmail.com>
2017-09-27 16:42:03 -0600

    improving the over function, is this really all ive done all day? jesus

--- R/Meta analysis summary/geospatial_analysis_and_plotting.R
+++ R/Meta analysis summary/geospatial_analysis_and_plotting.R
@@ -5,6 +5,7 @@ library(magrittr)
 library(ggmap)
 library(broom)
 library(maptools)
+library(rgeos)
 # install.packages("gpclib", type="source")
 # gpclibPermit()
 
@@ -46,26 +47,32 @@ mp <- ggplot(eco.points)+aes(long,lat,group=group)
 
 # Which polygon is our point in? ------------------------------------------
 library(tidyverse)
-load('C:/Users/Alculus/Documents/Bozeman/BZDEL/Data/MetaAnalysis/seroprevalence.Rdata')
+load("~/Desktop/BDEL/BZDEL/Data/MetaAnalysis/seroprevalence.Rdata")
 
-ecos <- shapefile('official_teow/wwf_terr_ecos.shp')
+ecos <- shapefile('~/Desktop/BDEL/BZDEL/Data/MetaAnalysis/official_teow/wwf_terr_ecos.shp')
 
 # xy <- seroprevalence[,c('north','east')]
 # colnames(xy) <- c('lat','long')
 # xy <- xy[(!(is.na(xy$lat)|is.na(xy$long))),] %>% as.data.frame
 
 xy = seroprevalence %>%
-  mutate(lat = ave(north, south, rm.na=TRUE)) %>%
-  mutate(lon = ave(west, east, rm.na=TRUE)) %>%
+  mutate(lat = ave(north_final, south_final, rm.na=TRUE)) %>%
+  mutate(lon = ave(west_final, east_final, rm.na=TRUE)) %>%
   select(c(lon, lat)) %>%
   #rename(lat=north,long=east) %>% 
-  filter(!(is.na(lat))) %>%
-  filter(!(is.na(lon))) %>%
+  #filter(!(is.na(lat))) %>%
+  #filter(!(is.na(lon))) %>%
+  mutate(lon = ifelse(is.na(lon), 10, lon)) %>%
+  mutate(lat = ifelse(is.na(lat), 10, lat)) %>%
   as.data.frame
+
+#we should probably go back and use polygons over polygons, but that is proving way too hard rn
 
 spdf <- SpatialPointsDataFrame(coords = xy,data=xy,
                                proj4string = CRS(proj4string(ecos)))
 
 pps <- over(spdf,ecos[,'ECO_NAME'])
 
+seroprevalence_x <- as.data.frame(c(seroprevalence, pps)) %>%
+  select(-c(north, south, west, east, administrative_area_level_1, north_two, south_two, west_two, east_two))
 
