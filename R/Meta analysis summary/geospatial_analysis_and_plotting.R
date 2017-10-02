36a6dbb189282d4877165f0724abc53da463dceb
Crowley <r83c996@msu.montana.edu>
2017-10-02 09:18:30 -0700

    some improvements, but we need to remove the ggplotly

--- R/Meta analysis summary/geospatial_analysis_and_plotting.R
+++ R/Meta analysis summary/geospatial_analysis_and_plotting.R
@@ -1,23 +1,18 @@
 ### mapping samples to raster polygons
 library(raster)
 library(rgdal)
-library(magrittr)
+library(tidyverse)
 library(ggmap)
 library(broom)
 library(maptools)
 library(rgeos)
+library(plotly)
+
 # install.packages("gpclib", type="source")
 # gpclibPermit()
 
 #ecos <- shapefile('official_teow/wwf_terr_ecos.shp')
 #cols <- rainbow(length(unique(ecos$BIOME))) %>% sample(.)
-
-plot(ecos)
-i=0
-for (biom in unique(ecos$BIOME)){
-  i=i+1
-  plot(ecos[ecos$BIOME==biom,],col=cols[i],add=TRUE)
-}
 
 
 ### alternatively, using maptools & ggmap
@@ -36,7 +31,7 @@ ggplot()+
   coord_fixed(1.3)
 
 mp <- ggplot(eco.points)+aes(long,lat,group=group)
-
+mp
 # 
 # worldmap <- borders('world',colour = 'grey50',fill='grey50')
 # 
@@ -50,29 +45,6 @@ library(tidyverse)
 load("~/BZDEL/Data/MetaAnalysis/seroprevalence.Rdata")
 
 ecos <- shapefile('~/BZDEL/Data/MetaAnalysis/official_teow/wwf_terr_ecos.shp')
-
-# xy <- seroprevalence[,c('north','east')]
-# colnames(xy) <- c('lat','long')
-# xy <- xy[(!(is.na(xy$lat)|is.na(xy$long))),] %>% as.data.frame
-
-xy = seroprevalence %>%
-  mutate(lat = ave(north_final, south_final, rm.na=TRUE)) %>%
-  mutate(lon = ave(west_final, east_final, rm.na=TRUE)) %>%
-  select(c(lon, lat)) %>%
-  #rename(lat=north,long=east) %>% 
-  #filter(!(is.na(lat))) %>%
-  #filter(!(is.na(lon))) %>%
-  mutate(lon = ifelse(is.na(lon), 10, lon)) %>%
-  mutate(lat = ifelse(is.na(lat), 10, lat)) %>%
-  as.data.frame
-
-spdf <- SpatialPointsDataFrame(coords = xy,data=xy,
-                               proj4string = CRS(proj4string(ecos)))
-
-pps <- over(spdf,ecos[,'ECO_NAME'])
-
-seroprevalence_x <- as.data.frame(c(seroprevalence, pps)) %>%
-  select(-c(north, south, west, east, administrative_area_level_1, north_two, south_two, west_two, east_two))
 
 #we should probably go back and use polygons over polygons, but that is proving way too hard rn
 
@@ -124,15 +96,29 @@ map <- get_map(location = c(lon = mean(lon), lat = mean(lat)), zoom = 2,
                maptype = "satellite", source = "google")
 
 coordinate_box_fortified <- fortify(m)
-
+eco.points = fortify(ecos)
+ecos2 = tidy(ecos)
+eco.points2 = fortify(ecos2)
 ### When you draw a figure, you limit lon and lat.      
 ggmap(map)+
   scale_x_continuous(limits = c(min(seroprevalence_x_final$west_final),max(seroprevalence_x_final$east_final))) +
   scale_y_continuous(limits = c(min(seroprevalence_x_final$west_final),max(seroprevalence_x_final$east_final))) +
-  geom_polygon(aes(x=long, y=lat, group=group), fill='grey', size=.2,color='green', data=coordinate_box_fortified, alpha=.3)
+  #geom_polygon(aes(x=long, y=lat, group=group), fill='grey', size=.2,color='green', data=coordinate_box_fortified, alpha=.3) #+
+  geom_polygon(data=eco.points,aes(x=long,y=lat,group=group,fill=group))
+
+foo <- ggplot()+
+  geom_polygon(data=world,aes(x=long,y=lat,group=group))+
+  geom_polygon(data=eco.points,aes(x=long,y=lat,group=group,fill=group))+
+  theme(legend.position = 'none')+
+  coord_fixed(1.3) +
+  scale_x_continuous(limits = c(min(seroprevalence_x_final$west_final),max(seroprevalence_x_final$east_final))) +
+  scale_y_continuous(limits = c(min(seroprevalence_x_final$west_final),max(seroprevalence_x_final$east_final))) +
+  geom_polygon(aes(x=long, y=lat, group=group), fill='grey', size=.5,color='black', data=coordinate_box_fortified, alpha=.5) #+
+
+ggplotly(foo)
 
 
-
-
-
-
+y <- seroprevalence_x_final %>%
+  dplyr::select(c(title, sampling_location, coordinate_box)) %>%
+  unique()
+  
