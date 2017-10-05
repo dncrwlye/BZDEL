ceb7210582fe85a6480828a87fbf7aa0558ad6e4
Crowley <r83c996@msu.montana.edu>
2017-10-05 16:29:05 -0700

    wtf is happening, find out later

--- R/Meta analysis summary/birth_pulse_analyses.R
+++ R/Meta analysis summary/birth_pulse_analyses.R
@@ -16,8 +16,8 @@ seroprevalence_single_time_point <- seroprevalence_x_final %>%
     mutate(month = as.numeric(format(month, "%m"))) %>%
     mutate(year = as.numeric(format(sampling_date_single_time_point, "%Y"))) %>%
     #mutate(month = format(as.Date(sampling_date_single_time_point), "%m-%d"))) %>%
-    unite(group, c(age_class, last_name_of_first_author, year), sep = ", ") %>%
-    group_by(title, month, virus, species, ECO_NAME,  methodology, group) %>%
+    mutate(substudy = paste(title, year, species, sep = ', ')) %>%
+    group_by(title, month, virus, species, ECO_NAME,  methodology, age_class, last_name_of_first_author, year, sampling_location) %>%
     summarise(sample_size = sum(sample_size), successes = sum (successes))  %>%
     mutate(seroprevalence_per = 100 * (successes/sample_size)) %>%
     filter(!is.na(seroprevalence_per)) %>%
@@ -34,8 +34,8 @@ seroprevalence_unclear_time_point <- seroprevalence_x_final %>%
     mutate(month_1 = ifelse(difftime < 364, as.numeric(format(start_of_sampling, "%m")), 1)) %>%
     mutate(month_2 = ifelse(difftime < 364, as.numeric(format(end_of_sampling, "%m")), 12)) %>%
     mutate(year = as.numeric(format(start_of_sampling, "%Y"))) %>%
-    unite(group, c(age_class, last_name_of_first_author, year), sep = ", ") %>%
-    group_by(title, month_1, month_2, virus, species, ECO_NAME, methodology, group) %>%
+    #unite(group, c(age_class, last_name_of_first_author, year), sep = ", ") %>%
+    group_by(title, month_1, month_2, virus, species, ECO_NAME, methodology, age_class, last_name_of_first_author, year, sampling_location) %>%
     #group_by(month_1, month_2, age_class, species, virus, country, title, last_name_of_first_author, methodology, global_location, sampling_location) %>%
     summarise(sample_size = sum(sample_size), successes = sum (successes),difftime = mean(difftime)) %>%
     mutate(difftime_sqrt = sqrt(difftime)) %>% #going to divide any long sampling period by the square root of the sampling period
@@ -48,8 +48,8 @@ seroprevalence_unclear_time_point <- seroprevalence_x_final %>%
     mutate(seroprevalence_per_lower_bound = ifelse(difftime > 32, seroprevalence_per_lower_bound/difftime_sqrt, seroprevalence_per_lower_bound)) %>%
     ungroup() %>%
     dplyr::select(-c(difftime, difftime_sqrt)) %>%
-    mutate(group = paste(group, "xc")) %>%
-    ungroup()
+    mutate(title = paste(title, "xc")) 
+   
   
   x <- seroprevalence_unclear_time_point[,c(1:2,4:ncol(seroprevalence_unclear_time_point))]
   y <-seroprevalence_unclear_time_point[,c(1,3:ncol(seroprevalence_unclear_time_point))]
@@ -71,13 +71,13 @@ Bat_Birth_Pulse_Data <- Bat_Birth_Pulse_Data %>%
 
 seroprevalence_graph_inner_join <- inner_join(seroprevalence_graph, Bat_Birth_Pulse_Data_final)
 
-seroprevalence_graph_inner_join_filter.analyses <- seroprevalence_graph_inner_join %>%
-  dplyr::select(species, group,ECO_NAME, month, birth_pulse_1_quant) %>%
-  unique() %>%
-  group_by(species, group) %>%
-  summarise(sd = sd(birth_pulse_1_quant, na.rm=TRUE)) %>%
-  mutate(sd_total = sd(seroprevalence_graph_inner_join$birth_pulse_1_quant, na.rm=TRUE)) %>%
-  mutate(ratio = sd/sd_total)
+#seroprevalence_graph_inner_join_filter.analyses <- seroprevalence_graph_inner_join %>%
+#  dplyr::select(species, group,ECO_NAME, month, birth_pulse_1_quant) %>%
+#  unique() %>%
+#  group_by(species, group) %>%
+#  summarise(sd = sd(birth_pulse_1_quant, na.rm=TRUE)) %>%
+#  mutate(sd_total = sd(seroprevalence_graph_inner_join$birth_pulse_1_quant, na.rm=TRUE)) %>%
+#  mutate(ratio = sd/sd_total)
 
 
 seroprevalence_graph_inner_join_filter <- seroprevalence_graph_inner_join %>%
@@ -88,7 +88,8 @@ seroprevalence_graph_inner_join_filter <- seroprevalence_graph_inner_join %>%
   mutate(month.dc.birthpulse = month - birth_pulse_1_quant_new) 
 
 seroprevalence_graph_inner_join_filter_alex_style <- seroprevalence_graph_inner_join_filter %>%
-  unite(substudy, c(group, species, title), sep = ", ") %>%
+  #unite(substudy, c(year, species, title), sep = ", ") %>%
+  mutate(substudy = paste(title, year, species,sampling_location, sep = ', ')) %>%
   group_by(substudy) %>%
   mutate(value = (seroprevalence_per - mean(seroprevalence_per))/sd(seroprevalence_per, na.rm=TRUE))
   
@@ -113,6 +114,16 @@ x<-seroprevalence_graph_inner_join_filter_alex_style %>%
 
 
 ggplotly(x, tooltip = 'text')
+
+
+
+
+x<-seroprevalence_x_final %>%
+  filter(title == 'Antibodies to Nipah or Nipah-like Viruses in Bats , China' & species == "hipposideros pomona")
+
+
+
+
 
 
 seroprevalence_graph_anti_join <- anti_join(seroprevalence_graph, Bat_Birth_Pulse_Data_final)
