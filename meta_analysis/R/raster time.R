30b1bfe146332aeb2bb7e4ff348f69f5335ca40c
Dan Crowley <dancrowley.e@gmail.com>
2017-11-16 10:25:36 -0700

    caught some mistakes

--- meta_analysis/R/raster time.R
+++ meta_analysis/R/raster time.R
@@ -29,24 +29,25 @@ MetaAnalysis_Data_New_Version <- read_excel("~/Dropbox_gmail/Dropbox/bat virus m
 seroprevalence <- MetaAnalysis_Data_New_Version %>%
   filter(outcome == 'Seroprevalence') %>%
   dplyr::select(title, last_name_of_first_author, virus, study_type, study_design, methodology, species, sex, age_class, sampling_location, sampling_location_two, sample_size, seroprevalence_percentage, single_sampling_point, sampling_date_single_time_point, start_of_sampling, end_of_sampling) %>%
-  mutate(virus = ifelse((virus == "Ebola" | 
-                           virus == "Marburg" | 
-                           virus == "Zaire Ebola"|
-                           virus == "Sudan virus" | 
-                           virus == "Zaire Ebolavirus" | 
-                           virus == "Zaire Ebola"|
-                           virus == "Reston Ebola"), "Filovirus", 
-                        ifelse(virus  == "Nipah" | virus == "Hendra" | virus  == "Henipavirus", "Henipavirus", virus))) %>%
+  mutate(virus = ifelse(grepl('Ebola|Marburg|Sudan', virus), 'Filovirus',
+                 ifelse(grepl('Henipa|Hendra|Nipah', virus), 'Henipavirus',
+                 ifelse(grepl('Tioman', virus), 'Tioman', virus)))) %>%
   mutate(sampling_location = tolower(sampling_location)) %>%
   mutate(country = stri_extract_first_regex(sampling_location, '[a-z]+')) %>%
   mutate(species = tolower(species)) %>%
   mutate(species = trimws(species)) %>%
   mutate(species = stri_extract_first_regex(species, '[a-z]+ [a-z]+')) %>%
-  mutate(methodology = ifelse(methodology == 'PCR'| methodology == 'RT-PCR'| methodology == "RT-PCR  (urine)"| methodology == "RT-PCR (Oro-pharangyeal swab)", 'PCR based method', 
-                              ifelse(methodology == "ELISA" | methodology == "ELISA + WB" | methodology == "Luminex", 'non nAb based method',
-                                     ifelse(methodology == "VNT"| methodology == "SNT"| methodology == "Unclear, Presumably Neutralizing Antibodies", "nAb based method", methodology)))) %>%
+  mutate(methodology = ifelse(grepl('PCR', methodology), 'PCR based method',
+                          ifelse(grepl('ELISA|Luminex', methodology), 'non nAb based method',
+                          ifelse(grepl('VNT|SNT|Neutralizing', methodology), "nAb based method", methodology)))) %>%
+  #mutate(methodology_old = ifelse(methodology == 'PCR'| methodology == 'RT-PCR'| methodology == "RT-PCR  (urine)"| methodology == "RT-PCR (Oro-pharangyeal swab)", 'PCR based method', 
+  #                            ifelse(methodology == "ELISA" | methodology == "ELISA + WB" | methodology == "Luminex", 'non nAb based method',
+  #                                   ifelse(methodology == "VNT"| methodology == "SNT"| methodology == "Unclear, Presumably Neutralizing Antibodies", "nAb based method", methodology)))) %>%
   mutate(successes = round((1/100)*seroprevalence_percentage * sample_size,0)) 
  
+table(seroprevalence$virus, seroprevalence$virus.dc)
+
+
 seroprevalence_search <- as.data.frame(unique(seroprevalence$sampling_location)) %>%
   rename(sampling_location = `unique(seroprevalence$sampling_location)`) %>%
   mutate(sampling_location = as.character(sampling_location)) %>%
