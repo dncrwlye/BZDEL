da3e5f569de932984455b718e6be3f8558586038
Dan Crowley <dancrowley.e@gmail.com>
2017-12-01 12:06:29 -0700

    takes the excel data and creates seroprevalence.Rdata

--- meta_analysis/R/seroprevalence_clean_script.R
+++ meta_analysis/R/seroprevalence_clean_script.R
@@ -56,22 +56,74 @@ seroprevalence <- seroprevalence %>%
   group_by_at(vars(-seroprevalence_percentage, -successes, -sample_size)) %>%
   summarise(seroprevalence_percentage.dc = weighted.mean(seroprevalence_percentage, w = sample_size), sample_size.dc = mean(sample_size)) %>%
   dplyr::rename(seroprevalence_percentage = seroprevalence_percentage.dc) %>%
-  dplyr::rename(sample_size = sample_size.dc) 
+  dplyr::rename(sample_size = sample_size.dc) %>%
+  ungroup()
 
+#...........adding on a column for becker, unfortunately many papers did multiple methods.....
 
-# x<-seroprevalence%>%
-#   filter(title=='Prevalence of Henipavirus and Rubulavirus Antibodies in Pteropid Bats, Papau New Guinea')
+seroprevalence <- seroprevalence %>%
+  mutate(date_diff = end_of_sampling - start_of_sampling) %>%
+  mutate(date_diff_cat = ifelse(single_sampling_point == 1 | date_diff < 365/12, 'not horrible', 
+                         ifelse(single_sampling_point == 0 | date_diff > 365/12, 'horrible', NA))) %>%
+  mutate(substudy_non_annual = paste(title, study_design, species, sex, methodology, age_class, sampling_location, sep = ', '))
+  
+explicit_longitudinal<-seroprevalence %>%
+  mutate(date_diff = end_of_sampling - start_of_sampling) %>%
+  filter(single_sampling_point == 1 | date_diff < 365/12) %>%
+  dplyr::select(substudy_non_annual, sampling_date_single_time_point, start_of_sampling) %>%
+  unique() %>%
+  group_by(substudy_non_annual) %>%
+  summarise(counts= n()) %>%
+  filter(counts >= 2) %>%
+  ungroup() %>%
+  #dplyr::select(title) %>%
+  unique()
 
-seroprevalence_search <- as.data.frame(unique(seroprevalence$sampling_location)) %>%
-  rename(sampling_location = `unique(seroprevalence$sampling_location)`) %>%
-  mutate(sampling_location = as.character(sampling_location)) %>%
-  mutate(north=NA) %>%
-  mutate(south=NA) %>%
-  mutate(west=NA) %>%
-  mutate(east=NA) %>%
-  mutate(address = NA) 
+single_time_points_but_decent_range<-seroprevalence %>%
+  mutate(date_diff = end_of_sampling - start_of_sampling) %>%
+  filter(single_sampling_point == 1 | date_diff < 365/12) %>%
+  dplyr::select(substudy_non_annual, sampling_date_single_time_point, start_of_sampling) %>%
+  unique() %>%
+  group_by(substudy_non_annual) %>%
+  summarise(counts= n()) %>%
+  filter(counts == 1) %>%
+  ungroup() %>%
+  #dplyr::select(title) %>%
+  unique()
 
-seroprevalence <- seroprevalence %>% unique()
+pooled_estimates_just_horrible<-seroprevalence %>%
+  mutate(date_diff = end_of_sampling - start_of_sampling) %>%
+  filter(single_sampling_point == 0 & date_diff >= 365/12) %>%
+  dplyr::select(substudy_non_annual, sampling_date_single_time_point, start_of_sampling, date_diff) %>%
+  unique() %>%
+  group_by(substudy_non_annual, date_diff) %>%
+  summarise(counts= n()) %>%
+  ungroup() %>%
+  unique()
+
+# sum(pooled_estimates_just_horrible$counts) + sum(explicit_longitudinal$counts) + sum(single_time_points_but_decent_range$counts)
+# nrow(seroprevalence)
+# xy <- seroprevalence %>%
+#   filter(!(seroprevalence$substudy_non_annual %in% explicit_longitudinal$substudy_non_annual) &
+#          !(seroprevalence$substudy_non_annual %in% single_time_points_but_decent_range$substudy_non_annual) &
+#          !(seroprevalence$substudy_non_annual %in% pooled_estimates_just_horrible$substudy_non_annual))
+# l <- seroprevalence %>%
+#   filter((seroprevalence$substudy_non_annual %in% explicit_longitudinal$substudy_non_annual))
+# m <- seroprevalence %>%
+#   filter((seroprevalence$substudy_non_annual %in% single_time_points_but_decent_range$substudy_non_annual))
+# n <- seroprevalence %>%
+#   filter((seroprevalence$substudy_non_annual %in% pooled_estimates_just_horrible$substudy_non_annual))
+# l$substudy_non_annual %in% m$substudy_non_annual 
+# l$substudy_non_annual %in% n$substudy_non_annual
+# m$substudy_non_annual %in% n$substudy_non_annual
+# l.x <- l %>%
+#   filter(l$substudy_non_annual %in% n$substudy_non_annual)
+
+seroprevalence <- seroprevalence %>%
+  mutate(study_type = ifelse(substudy_non_annual %in% explicit_longitudinal$substudy_non_annual, 'explicit_longitudinal',
+                      ifelse(substudy_non_annual %in% single_time_points_but_decent_range$substudy_non_annual, 'single_time_points_but_decent_range',
+                      ifelse(substudy_non_annual %in% pooled_estimates_just_horrible$substudy_non_annual, 'pooled_estimates_just_horrible', NA )))) %>%
+  dplyr::select(-c(substudy_non_annual, date_diff, date_diff_cat)) %>%
+  unique()
 
 save(seroprevalence, file='data/seroprevalence.Rdata')
-
