7c52f345696b23d5553fc47ad155e6a91f1fd348
Dan Crowley <dancrowley.e@gmail.com>
2017-12-19 16:48:58 -0700

    introduce some species cleaning here

--- meta_analysis/R/birth_pulse_summary_data_cleaning.R
+++ meta_analysis/R/birth_pulse_summary_data_cleaning.R
@@ -12,6 +12,25 @@ library(broom)
 Bat_Birth_Pulse_Data <- read_excel("~/Dropbox_gmail/Dropbox/bat virus meta-analysis/bat birth pulse/Bat Birth Pulse Data.xlsx")
 
 Bat_Birth_Pulse_Data <- Bat_Birth_Pulse_Data %>%
+  mutate(species = tolower(species)) %>%
+  mutate(species = trimws(species)) %>%
+  mutate(species = gsub('epomorphus' ,"epomophorus", species)) %>%
+  #mutate(species = gsub('hipperosiderus' ,"hipposideros", species)) %>%
+  #mutate(species = gsub('megarops' ,"megaerops", species)) %>%
+  mutate(species = gsub('schreibersi' ,"schreibersii", species)) %>%
+  mutate(species = gsub('schreibersiii' ,"schreibersii", species)) %>%
+  mutate(species = gsub('minopterus' ,"miniopterus", species)) %>%
+  mutate(species = gsub('myonicterus' ,"myonycteris", species)) %>%
+  mutate(species = gsub('jagoli' ,"jagori", species)) %>%
+  mutate(species = gsub('leschenaulti' ,"leschenaultii", species)) %>%
+  mutate(species = gsub('leschenaultiii' ,"leschenaultii", species)) %>%
+  mutate(species = gsub('khuli' ,"kuhlii", species)) %>%
+  mutate(species = gsub('roussetus' ,"roussettus", species)) %>%
+  mutate(species = gsub('lavartus' ,"larvatus", species)) 
+
+write.csv(Bat_Birth_Pulse_Data, "~/Dropbox_gmail/Dropbox/bat virus meta-analysis/bat birth pulse/Bat Birth Pulse Data_final.csv")
+
+Bat_Birth_Pulse_Data <- Bat_Birth_Pulse_Data %>%
   dplyr::select(c(species,location, birth_pulse_1_quant, birth_pulse_2_quant)) %>%
   #rename(country= location) %>%
   mutate(location = tolower(location)) %>%
