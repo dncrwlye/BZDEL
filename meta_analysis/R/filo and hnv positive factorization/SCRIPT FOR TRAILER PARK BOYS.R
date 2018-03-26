ed7b45c92c31d2000a26cd516a05b4e6d99a33a2
Dan Crowley <dancrowley.e@gmail.com>
2018-03-26 14:35:51 -0600

    self explanatory

---
+++ meta_analysis/R/filo and hnv positive factorization/SCRIPT FOR TRAILER PARK BOYS.R
@@ -0,0 +1,10 @@
+#......Alex all you should need to do is change the working directory
+setwd('....BZDEL/meta_analysis')
+
+source('R/filo and hnv positive factorization/filo_factorization.R', echo=TRUE, print.eval=TRUE)
+source('R/filo and hnv positive factorization/filo_factorization no sampling effort.R')
+source('R/filo and hnv positive factorization/hnv_factorization.R')
+source('R/filo and hnv positive factorization/hnv_factorization no sampling effort.R')
+
+
+
