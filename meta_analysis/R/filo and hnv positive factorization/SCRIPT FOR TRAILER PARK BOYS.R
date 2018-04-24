5746fe995a3e9580085053687f7cfd0eb9531b5a
Crowley <r83c996@msu.montana.edu>
2018-04-24 16:38:23 -0600

    the african bat data, could be useful

--- meta_analysis/R/filo and hnv positive factorization/SCRIPT FOR TRAILER PARK BOYS.R
+++ meta_analysis/R/filo and hnv positive factorization/SCRIPT FOR TRAILER PARK BOYS.R
@@ -3,25 +3,25 @@
 
 setwd("C:/Users/r83c996/Documents/BZDEL/meta_analysis")
 
-ncores = 3
+ncores = 4
 tot.reps=500
 reps.per.worker=round(tot.reps/ncores)
 sampling_effort = TRUE
 source('R/filo and hnv positive factorization/filo_factorization.R', echo=TRUE, print.eval=TRUE)
 
-ncores = 7
+ncores = 4
 tot.reps=500
 reps.per.worker=round(tot.reps/ncores)
 sampling_effort = TRUE
 source('R/filo and hnv positive factorization/hnv.factorization.R')
 
-ncores = 7
+ncores = 4
 tot.reps=500
 reps.per.worker=round(tot.reps/ncores)
 sampling_effort = FALSE 
 source('R/filo and hnv positive factorization/filo_factorization no sampling effort.R')
 
-ncores = 7
+ncores = 4
 tot.reps=500
 reps.per.worker=round(tot.reps/ncores)
 sampling_effort = FALSE 
